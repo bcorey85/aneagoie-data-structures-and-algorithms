ff10d6b2a8085d64c176688661022d7c4f18e5ec
Brandon Corey <bcorey85@gmail.com>
2020-10-14 23:03:55 -0500

    update linked list to doubly linked list

--- LinkedList.js
+++ LinkedList.js
@@ -2,6 +2,7 @@ class Node {
 	constructor(value) {
 		this.value = value;
 		this.next = null;
+		this.previous = null;
 	}
 }
 
@@ -16,6 +17,8 @@ class LinkedList {
 		const node = new Node(value);
 
 		this.tail.next = node;
+		node.previous = this.tail;
+
 		this.tail = node;
 		this.length++;
 	}
@@ -24,6 +27,7 @@ class LinkedList {
 		const node = new Node(value);
 
 		node.next = this.head;
+		this.head.previous = node;
 		this.head = node;
 		this.length++;
 	}
@@ -48,27 +52,19 @@ class LinkedList {
 		}
 
 		const leader = this.traverseToNode(index - 1);
-		const holdingPointer = leader.next;
+		const follower = leader.next;
 
 		const node = new Node(value);
 		leader.next = node;
-		node.next = holdingPointer;
-	}
-
-	traverseToNode(index) {
-		let counter = 0;
-		let currentNode = this.head;
-		while (counter !== index) {
-			currentNode = currentNode.next;
-			counter++;
-		}
-
-		return currentNode;
+		node.previous = leader;
+		node.next = follower;
+		follower.previous = node;
 	}
 
 	remove(index) {
 		if (index <= 0) {
 			this.head = this.head.next;
+			this.head.previous = null;
 			this.length--;
 			return;
 		}
@@ -82,14 +78,28 @@ class LinkedList {
 		}
 
 		const leader = this.traverseToNode(index - 1);
-		const deletedNode = leader.next;
-		leader.next = deletedNode.next;
+		const follower = this.traverseToNode(index + 1);
+		follower.previous = leader;
+		leader.next = follower;
 		this.length--;
+	}
+
+	traverseToNode(index) {
+		let counter = 0;
+		let currentNode = this.head;
+		while (counter !== index) {
+			currentNode = currentNode.next;
+			counter++;
+		}
+
+		return currentNode;
 	}
 }
 
 const myLinkedList = new LinkedList(10);
+console.log(myLinkedList.printList());
 myLinkedList.append(5);
+console.log(myLinkedList.printList());
 myLinkedList.prepend(15);
 console.log(myLinkedList.printList());
 myLinkedList.insert(-50, 1);
