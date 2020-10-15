7f99f9d7d2fe0badd89aa0d34b23d50c23da3b2a
Brandon Corey <bcorey85@gmail.com>
2020-10-15 00:26:40 -0500

    add reversing linked lists

--- LinkedList.js
+++ LinkedList.js
@@ -1,8 +1,11 @@
 class Node {
-	constructor(value) {
+	constructor(value, config) {
 		this.value = value;
 		this.next = null;
-		this.previous = null;
+
+		if (config && config.double) {
+			this.previous = null;
+		}
 	}
 }
 
@@ -17,6 +20,109 @@ class LinkedList {
 		const node = new Node(value);
 
 		this.tail.next = node;
+		this.tail = node;
+		this.length++;
+	}
+
+	prepend(value) {
+		const node = new Node(value);
+
+		node.next = this.head;
+		this.head = node;
+		this.length++;
+	}
+
+	printList() {
+		const array = [];
+		let currentNode = this.head;
+		while (currentNode !== null) {
+			array.push(currentNode.value);
+			currentNode = currentNode.next;
+		}
+		return array;
+	}
+
+	insert(index, value) {
+		if (index <= 0) {
+			return this.prepend(value);
+		}
+
+		if (index >= this.length) {
+			return this.append(value);
+		}
+
+		const leader = this.traverseToNode(index - 1);
+		const holdingPointer = leader.next;
+
+		const node = new Node(value);
+		leader.next = node;
+		node.next = holdingPointer;
+	}
+
+	traverseToNode(index) {
+		let counter = 0;
+		let currentNode = this.head;
+		while (counter !== index) {
+			currentNode = currentNode.next;
+			counter++;
+		}
+
+		return currentNode;
+	}
+
+	reverse() {
+		if (!this.head.next) {
+			return this.head;
+		}
+
+		this.tail = this.head;
+
+		let first = this.head;
+		let second = first.next;
+		while (second) {
+			const third = second.next;
+			second.next = first;
+			first = second;
+			second = third;
+		}
+
+		this.head.next = null;
+		this.head = first;
+	}
+
+	remove(index) {
+		if (index <= 0) {
+			this.head = this.head.next;
+			this.length--;
+			return;
+		}
+
+		if (index >= this.length) {
+			const leader = this.traverseToNode(this.length - 1 - 1);
+			this.tail = leader;
+			this.tail.next = null;
+			this.length--;
+			return;
+		}
+
+		const leader = this.traverseToNode(index - 1);
+		const deletedNode = leader.next;
+		leader.next = deletedNode.next;
+		this.length--;
+	}
+}
+
+class DoubleLinkedList {
+	constructor(value) {
+		this.head = new Node(value, { double: true });
+		this.tail = this.head;
+		this.length = 1;
+	}
+
+	append(value) {
+		const node = new Node(value, { double: true });
+
+		this.tail.next = node;
 		node.previous = this.tail;
 
 		this.tail = node;
@@ -24,7 +130,7 @@ class LinkedList {
 	}
 
 	prepend(value) {
-		const node = new Node(value);
+		const node = new Node(value, { double: true });
 
 		node.next = this.head;
 		this.head.previous = node;
@@ -54,7 +160,7 @@ class LinkedList {
 		const leader = this.traverseToNode(index - 1);
 		const follower = leader.next;
 
-		const node = new Node(value);
+		const node = new Node(value, { double: true });
 		leader.next = node;
 		node.previous = leader;
 		node.next = follower;
@@ -84,6 +190,29 @@ class LinkedList {
 		this.length--;
 	}
 
+	reverse() {
+		if (!this.head.next) {
+			return this.head;
+		}
+
+		this.tail = this.head;
+		this.tail.previous = this.tail.next;
+		let first = this.head;
+		let second = first.next;
+		[ 5, 10, 15, 1 ];
+
+		while (second) {
+			const third = second.next;
+			second.next = first;
+			second.previous = third;
+			first = second;
+			second = third;
+		}
+
+		this.head.next = null;
+		this.head = first;
+	}
+
 	traverseToNode(index) {
 		let counter = 0;
 		let currentNode = this.head;
@@ -97,12 +226,14 @@ class LinkedList {
 }
 
 const myLinkedList = new LinkedList(10);
-console.log(myLinkedList.printList());
 myLinkedList.append(5);
-console.log(myLinkedList.printList());
 myLinkedList.prepend(15);
-console.log(myLinkedList.printList());
 myLinkedList.insert(-50, 1);
-console.log(myLinkedList.printList());
 myLinkedList.remove(2);
-console.log(myLinkedList.printList());
+myLinkedList.reverse();
+
+const myDoubleLinkedList = new DoubleLinkedList(10);
+myDoubleLinkedList.append(5);
+myDoubleLinkedList.prepend(15);
+myDoubleLinkedList.insert(-50, 1);
+myDoubleLinkedList.reverse();
